cf3ecbd51e48fb9225a3d67c3f551e52962af994
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2025-01-03 19:29:24 +0900

    カード素材

--- CenterMonitor/Assets/Scripts/ModeBoot.cs
+++ CenterMonitor/Assets/Scripts/ModeBoot.cs
@@ -15,15 +15,15 @@ namespace PokerTableVisualizer
         #region Mode
         public Mode.Control init()
         {
-            _SerialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
-            _SerialPort.Open();
+//            _SerialPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+//            _SerialPort.Open();
             return Mode.Control.Done;
         }
 
         public Mode.Control update()
         {
-            var line = _SerialPort.ReadLine();
-            Log.info(this, line);
+//            var line = _SerialPort.ReadLine();
+  //          Log.info(this, line);
             return Mode.Control.Keep;
         }
 
