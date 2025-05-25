966832d2dfe3c4244920fe648de59c16269a1647
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2025-05-25 18:08:34 +0900

    プレイヤーの有効/無効管理、ボタン管理

--- MonitorBrowser/PokerTableMonitor.js
+++ MonitorBrowser/PokerTableMonitor.js
@@ -18,6 +18,7 @@ PokerTableMonitor.engine = (function(){{
 	var HTML_ID_PLAYER_VIEEPANEL_PREFIX = "view_panel_player_";
 	var HTML_ID_SECTOR_GLOBAL = "sector_global";
 	var HTML_CLASS_PANEL_PLAYER = "panel_player";
+
 	var HTML_CLASS_PANEL_PLAYER_NAME_VALUE = "panel_player_name_value";
 	var HTML_CLASS_PANEL_PLAYER_HAND_VALUE = "panel_player_hand_value";
 	var HTML_CLASS_PANEL_PLAYER_HAND_IMG_STYLE = "panel_player_hand_image_style";
@@ -27,8 +28,51 @@ PokerTableMonitor.engine = (function(){{
 	var HTML_CLASS_PANEL_PLAYER_LOG_HAND = "panel_player_log_hand";
 	var HTML_CLASS_PANEL_PLAYER_LOG_ACTION = "panel_player_log_action";
 	var HTML_CLASS_PANEL_PLAYER_LOG_HAND_IMG_STYLE = "panel_player_log_hand_image_style";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_VALUE = "panel_player_position_value";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_EXIST = "panel_player_position_exist";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_TRUE = "panel_player_position_exist_alive";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_FALSE = "panel_player_position_exist_seat_open";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_NAME = "panel_player_position_name";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_DEALER = "panel_player_position_dealer_button";
+	var HTML_CLASS_PANEL_PLAYER_POSITION_DEALER_HAVE = "panel_player_position_dealer_button_have";
 
-	
+	// ポジション名
+	var POKER_POSITION_DEALER = "D";
+	var POKER_POSITION_CUTOFF = "CO";
+	var POKER_POSITION_HIJACK = "HJ";
+	var POKER_POSITION_MIDDLEp1 = "MP+1";
+	var POKER_POSITION_MIDDLE = "MP";
+	var POKER_POSITION_UTGp2 = "UTG+2";
+	var POKER_POSITION_UTGp1 = "UTG+1";
+	var POKER_POSITION_UTG = "UTG";
+	var POKER_POSITION_BB = "BB";
+	var POKER_POSITION_SB = "SB";
+	var POKER_POSITION_OPENSEAT = "-";
+
+	// ポジションリスト。ポジションの悪い方から
+	var POKER_POSITION_TABLE = [
+		// あると便利なので定義してるだけ
+		[POKER_POSITION_OPENSEAT],
+		[POKER_POSITION_DEALER],
+		// ヘッズアップ
+		[POKER_POSITION_BB, POKER_POSITION_DEALER],
+		// 3 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_DEALER],
+		// 4 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_DEALER],
+		// 5 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_MIDDLE, POKER_POSITION_DEALER],
+		// 6 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_HIJACK, POKER_POSITION_CUTOFF, POKER_POSITION_DEALER],
+		// 7 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_MIDDLE, POKER_POSITION_HIJACK, POKER_POSITION_CUTOFF, POKER_POSITION_DEALER],
+		// 8 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_UTGp1, POKER_POSITION_MIDDLE, POKER_POSITION_HIJACK, POKER_POSITION_CUTOFF, POKER_POSITION_DEALER],
+		// 9 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_UTGp1, POKER_POSITION_MIDDLE, POKER_POSITION_MIDDLEp1, POKER_POSITION_HIJACK, POKER_POSITION_CUTOFF, POKER_POSITION_DEALER],
+		// 10 名
+		[POKER_POSITION_SB, POKER_POSITION_BB, POKER_POSITION_UTG, POKER_POSITION_UTGp1, POKER_POSITION_UTGp2, POKER_POSITION_MIDDLE, POKER_POSITION_MIDDLEp1, POKER_POSITION_HIJACK, POKER_POSITION_CUTOFF, POKER_POSITION_DEALER],
+	];
 
 	// リソースまわり
 	var ASSET_ROOT = "../Assets/UI";
@@ -68,6 +112,15 @@ PokerTableMonitor.engine = (function(){{
 		else
 		{
 			return null;
+		}
+	}
+
+	// エレメントにイベントを追加する
+	function addEventListenerToElement(element, name, event)
+	{
+		if(element)
+		{
+			element.addEventListener(name, event);
 		}
 	}
 
@@ -156,18 +209,79 @@ PokerTableMonitor.engine = (function(){{
 		this.ElementNameValue = searchNodeByClassNameFromChildren(argCloneHtmlNode, HTML_CLASS_PANEL_PLAYER_NAME_VALUE, "");
 		this.ElementHandValue = searchNodeByClassNameFromChildren(argCloneHtmlNode, HTML_CLASS_PANEL_PLAYER_HAND_VALUE, "");
 		this.ElementLogValue = searchNodeByClassNameFromChildren(argCloneHtmlNode, HTML_CLASS_PANEL_PLAYER_LOG_VALUE, "");
+		this.ElementPositionValue = searchNodeByClassNameFromChildren(argCloneHtmlNode, HTML_CLASS_PANEL_PLAYER_POSITION_VALUE);
+		this.ElementPositionExistValue = searchNodeByClassNameFromChildren(this.ElementPositionValue, HTML_CLASS_PANEL_PLAYER_POSITION_EXIST);
+		this.ElementPositionNameValue = searchNodeByClassNameFromChildren(this.ElementPositionValue, HTML_CLASS_PANEL_PLAYER_POSITION_NAME);
+		this.ElementPositionDealerValue = searchNodeByClassNameFromChildren(this.ElementPositionValue, HTML_CLASS_PANEL_PLAYER_POSITION_DEALER);
+
+		addEventListenerToElement(this.ElementPositionExistValue, 'click', this.onClickExistButton.bind(this) );
+		addEventListenerToElement(this.ElementPositionDealerValue, 'click', this.onClickExistDealerButton.bind(this) );
+
 		this.CurrentHand = [];
 		this.HandHistory = [];
+		this.Name = "";
+		this.Alive = null;
+		this.Position = null;
+		this.BackPlayerCount = 0;
+		this.Button = false;
 	}
+	// 名前の設定
 	cPlayerViewPanel.prototype.setName = function(argName)
 	{
+		this.Name = argName;
 		this.ElementNameValue.innerHTML = argName;
 	}
+	// ハンドの設定
 	cPlayerViewPanel.prototype.setCurrentHand = function(argHand)
 	{
 		this.CurrentHand = argHand;
 		this.drawHand();
-
+	}
+	// ボタンの設定
+	cPlayerViewPanel.prototype.setButton = function(argFlag)
+	{
+		this.Button = argFlag;
+		if(this.Button)
+		{
+			this.ElementPositionDealerValue.classList.add(HTML_CLASS_PANEL_PLAYER_POSITION_DEALER_HAVE);
+		}
+		else
+		{
+			this.ElementPositionDealerValue.classList.remove(HTML_CLASS_PANEL_PLAYER_POSITION_DEALER_HAVE);
+		}
+	}
+	// フロップ以降、自分のアクション以降にプレイヤーが何人いるか？
+	cPlayerViewPanel.prototype.setBackPlayerCount = function(argCount)
+	{
+		this.BackPlayerCount = argCount;
+	}
+	// ポジションの設定
+	cPlayerViewPanel.prototype.setPosition = function(argPosition)
+	{
+		this.Position = argPosition;
+		this.ElementPositionNameValue.innerHTML = argPosition;
+	}
+	// いるかどうかの設定ボタン押されたときの動作
+	cPlayerViewPanel.prototype.onClickExistButton = function(argEvent)
+	{
+		if(this.Alive)
+		{
+			this.Alive = false;
+			this.ElementPositionExistValue.classList.remove(HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_TRUE);
+			this.ElementPositionExistValue.classList.add(HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_FALSE);
+		}
+		else
+		{
+			this.Alive = true;
+			this.ElementPositionExistValue.classList.remove(HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_FALSE);
+			this.ElementPositionExistValue.classList.add(HTML_CLASS_PANEL_PLAYER_POSITION_EXIST_TRUE);
+		}
+		engine.Manager.updatePosition();
+	}
+	// ディーラーボタン設定ボタンを押下されたときの動作
+	cPlayerViewPanel.prototype.onClickExistDealerButton = function(argEvent)
+	{
+		engine.Manager.setButton(this.Name);
 	}
 	cPlayerViewPanel.prototype.proceedNextHand = function(argHandCount)
 	{
@@ -234,8 +348,9 @@ PokerTableMonitor.engine = (function(){{
 	{
 		console.log("cManager: called.");
 		this.Probes = [];
-		this.PlayerViewPanelCollection = {};
+		this.PlayerViewPanelCollection = [];
 		this.HandCount = 1;
+		this.ActivePlayerCount = 0;
 
 		var panel_player_elements = document.getElementsByClassName(HTML_CLASS_PANEL_PLAYER);
 		if(panel_player_elements)
@@ -306,8 +421,7 @@ PokerTableMonitor.engine = (function(){{
 	// デバッグ用テストコマンド
 	cManager.prototype.onCommandTest = function()
 	{
-		console.log("test");
-		const name = "test" + Object.keys(this.PlayerViewPanelCollection).length;
+		const name = "test_" + Object.keys(this.PlayerViewPanelCollection).length;
 		const view_panel = this.createPlayerPanelView(name);
 		view_panel.setCurrentHand([1, 2]);
 	}
@@ -315,9 +429,10 @@ PokerTableMonitor.engine = (function(){{
 	cManager.prototype.createPlayerPanelView = function(name)
 	{
 		// すでに生成されているのならば、それを返す。
-		if(name in this.PlayerViewPanelCollection)
+		const search_panel = this.PlayerViewPanelCollection.find(x => x.Name == name);
+		if(search_panel)
 		{
-			return this.PlayerViewPanelCollection[name];
+			return search_panel;
 		}
 
 		// HTML 部分を生成する
@@ -328,10 +443,61 @@ PokerTableMonitor.engine = (function(){{
 		// オブジェクトで wrapping する。また必要な紐付けを行なう。
 		const view_panel = new cPlayerViewPanel(clone, this.PlayerPanelLogUnitTemplate);
 		view_panel.setName(name);
-		this.PlayerViewPanelCollection[name] = view_panel;
+		this.PlayerViewPanelCollection.push(view_panel);
 		return view_panel;
 	}
 
+	// ボタンをセットする
+	cManager.prototype.setButton = function(name)
+	{
+		for(const panel of this.PlayerViewPanelCollection)
+		{
+			panel.setButton(panel.Name == name);
+		}
+		this.updatePosition();
+	}
+	// ポジションを更新する
+	cManager.prototype.updatePosition = function()
+	{
+		var button_index = this.PlayerViewPanelCollection.findIndex(x => x.Button);
+		if(button_index < 0)
+		{
+			return;
+		}
+
+		// BackPlayerCount の計算
+		const view_count = this.PlayerViewPanelCollection.length;
+		this.ActivePlayerCount = 0;
+		for(let index=0; index<view_count; ++index)
+		{
+			const access_index = (index + button_index) % view_count;
+			const panel = this.PlayerViewPanelCollection[access_index];
+			if(panel.Alive)
+			{
+				panel.setBackPlayerCount(this.ActivePlayerCount);
+				this.ActivePlayerCount++;
+			}
+			else
+			{
+				panel.setBackPlayerCount(-1);
+			}
+		}
+
+		// ポジション設定
+		const position_list = POKER_POSITION_TABLE[this.ActivePlayerCount];
+		for(let index=0; index<view_count; ++index)
+		{
+			const panel = this.PlayerViewPanelCollection[index];
+			if(panel.Alive)
+			{
+				panel.setPosition(position_list[this.ActivePlayerCount - panel.BackPlayerCount - 1]);
+			}
+			else
+			{
+				panel.setPosition(POKER_POSITION_OPENSEAT);
+			}
+		}
+	}
 
 	// ---------------------------------------------------------------------
 	// engine オブジェクト
@@ -343,10 +509,7 @@ PokerTableMonitor.engine = (function(){{
 			function add_button_event_listener(id, event)
 			{
 				const element = document.getElementById(id);
-				if(element)
-				{
-					element.addEventListener('click', event);
-				}
+				addEventListenerToElement(element, 'click', event);
 			}
 
 			console.log("PokerTableMonitor init");
